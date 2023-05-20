a1edb713afb242d3655264bdcb21b62a07a06739
yu256 <122450013+yu256@users.noreply.github.com>
2023-05-20 22:05:11 +0900

    a06

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,3 +1,18 @@
+#[proconio::fastout]
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        q: usize,
+        a: [usize; n],
+        lr: [(usize, usize); q],
+    }
+
+    let mut sum: Vec<usize> = vec![0; n + 1];
+    for i in 0..n {
+        sum[i + 1] = sum[i] + a[i];
+    }
+
+    for (left, right) in lr {
+        println!("{}", sum[right] - sum[left - 1]);
+    }
 }
