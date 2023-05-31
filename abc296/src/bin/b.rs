8db4845f90da25bb13bf3899b48ea59a1aeca308
yu256 <122450013+yu256@users.noreply.github.com>
2023-05-31 20:21:33 +0900

    abc297b

--- abc296/src/bin/b.rs
+++ abc296/src/bin/b.rs
@@ -1,3 +1,23 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        s: [String; 8],
+    }
+
+    let mut column: usize = 0;
+    let mut row: usize = 0;
+
+    for i in 0..8 {
+        for (index, c) in s[i].chars().enumerate() {
+            if c == '*' {
+                column = index;
+                row = 8 - i;
+            }
+        }
+    }
+    println!("{}{}", get_char(column), row);
+}
+
+fn get_char(n: usize) -> char {
+    let chars: Vec<char> = String::from("abcdefgh").chars().collect();
+    chars[n]
 }
