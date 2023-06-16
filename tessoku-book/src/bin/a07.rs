22d83733962b3dee8c935f0587784d7d6b217004
yu256 <122450013+yu256@users.noreply.github.com>
2023-06-16 09:29:01 +0900

    a07

--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -1,3 +1,21 @@
+#[proconio::fastout]
 fn main() {
-    todo!();
+    proconio::input! {
+        d: usize,
+        n: usize,
+        lr: [(usize, usize); n],
+    }
+
+    let mut attendees = vec![0; d + 1];
+
+    for i in 0..n {
+        let (l, r) = lr[i];
+        for j in l..=r {
+            attendees[j] += 1;
+        }
+    }
+
+    for i in 1..=d {
+        println!("{}", attendees[i]);
+    }
 }
