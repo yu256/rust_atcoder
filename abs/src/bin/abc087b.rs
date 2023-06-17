dde0da88c2fe2a70f484f823853273ebee597c0d
yu256 <122450013+yu256@users.noreply.github.com>
2023-06-18 02:55:08 +0900

    abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,22 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        x: usize,
+    }
+
+    let mut ans = 0usize;
+
+    for i in 0..=a {
+        for j in 0..=b {
+            for k in 0..=c {
+                if 500 * i + 100 * j + 50 * k == x {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
