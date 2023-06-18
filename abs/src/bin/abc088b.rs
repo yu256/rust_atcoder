836ca57e3119b2e37d8974659ce6da211ab6548b
yu256 <122450013+yu256@users.noreply.github.com>
2023-06-18 09:20:11 +0900

    abc088b

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,3 +1,21 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        mut a: [usize; n],
+    }
+
+    a.sort();
+
+    let mut alice = 0usize;
+    let mut bob = 0usize;
+
+    for (i, c) in a.iter().rev().enumerate() {
+        if i % 2 == 0 {
+            alice += c;
+        } else {
+            bob += c;
+        }
+    }
+
+    println!("{}", alice - bob);
 }
