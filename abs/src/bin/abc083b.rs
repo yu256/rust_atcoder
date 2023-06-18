be80b3d70200baa247b0d94eee6edea96ba222bf
yu256 <122450013+yu256@users.noreply.github.com>
2023-06-18 09:04:54 +0900

    abc083b

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,3 +1,26 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: usize,
+        b: usize,
+    }
+
+    let mut sums = Vec::new();
+
+    for i in 1..=n {
+        let mut sum = 0;
+        let mut num = i;
+
+        while num > 0 {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        if a <= sum && sum <= b {
+            sums.push(i);
+        }
+    }
+
+    let ans: usize = sums.iter().sum();
+    println!("{}", ans);
 }
