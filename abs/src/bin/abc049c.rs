c5abf958690a90810dc99d8ac09c7be52a51c2c8
yu256 <122450013+yu256@users.noreply.github.com>
2023-06-18 12:37:50 +0900

    abc049c

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,3 +1,25 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        s: String,
+    }
+
+    println!("{}", if check(&s) {"YES"} else {"NO"});
+
+}
+
+fn check(s: &str) -> bool {
+    if s.is_empty() {
+        return true;
+    }
+
+    for word in vec!["dream", "dreamer", "erase", "eraser"] {
+        if s.starts_with(word) {
+            let rest = &s[word.len()..];
+            if check(rest) {
+                return true;
+            }
+        }
+    }
+
+    false
 }
