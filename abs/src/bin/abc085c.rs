60f88bbb0e01c8d9f187c7591090a9590a266511
yu256 <122450013+yu256@users.noreply.github.com>
2023-06-18 11:08:18 +0900

    abc085c

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,3 +1,25 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        y: usize,
+    }
+
+    let mut answer = None;
+
+    'outer: for a in 0..=n {
+        for b in 0..=n - a {
+            let c = n - a - b;
+            let total_amount = 10000 * a + 5000 * b + 1000 * c;
+
+            if total_amount == y {
+                answer = Some((a, b, c));
+                break 'outer;
+            }
+        }
+    }
+
+    match answer {
+        Some((a, b, c)) => println!("{} {} {}", a, b, c),
+        _ => println!("-1 -1 -1")
+    }
 }
